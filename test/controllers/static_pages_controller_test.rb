994885140e3c9a71cb6c00cd18ec44bdb9611bdf
airwoman <airwomanzhy@hotmail.com>
2015-08-16 08:41:46 +1000

    full title func

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | My new website"
+    assert_select "title", "My new website"
   end
 
   test "should get help" do
