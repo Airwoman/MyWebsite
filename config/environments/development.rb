3dfe796904df157c6a9f6880805324bfb02759ce
airwoman <airwomanzhy@hotmail.com>
2015-09-14 10:51:37 +1000

    password forget

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,10 +14,18 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'example.com'
-  config.action_mailer.default_url_options = {host:host}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings ={
+    :enable_starttls_auto => true,
+    :address              => "smtp.live.com",
+    :port                 => "587",
+    :domain               => "mail.live.com",
+    :user_name            => "airwomanzhy@hotmail.com",
+    :password             => "Zhouhuayu926",
+    :authentication       => :plain  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
